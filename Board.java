7a2f7d45d095017aa610a05fbfe69f9d623bd1fe
Himanshu Arora <himanshu.arora1111@gmail.com>
2024-05-18 02:17:30 +0300

    read a board from a file
    also a function to check if a position exists

---
+++ Board.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Board {
+    char[][] cell;
+    int rows = 0;
+    int cols = 0;
+
+	public boolean exists(int x, int y) {
+		if( 0 <= x & x <= rows &
+		 0 <= y & y <= cols)
+			return true;
+		return false;
+	}
+
+    public static Board readBoard (String path) throws FileNotFoundException {
+		Scanner file = new Scanner( new File ( path));
+		Board b = new Board();
+		while(file.hasNextLine()) {
+			b.rows++;
+			b.cols = file.nextLine().length();
+		}
+		file = new Scanner(new File(path));
+		b.cell = new char[b.rows][b.cols];
+
+		int row = 0; 
+		int col = 0;
+		while(file.hasNextLine()) {
+			String line = file.nextLine();
+			for(int ctr = 0; ctr < line.length(); ctr++) {
+				b.cell[row][col] = line.charAt(col);
+				col++;
+			}
+			row++;
+		}
+		return b;
+    }
+
+}
