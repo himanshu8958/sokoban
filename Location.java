e6beb84dc600f7f122af0ac757ddb24f7f7b9471
Himanshu Arora <himanshu.arora1111@gmail.com>
2024-05-19 04:11:19 +0300

    typically used locations

--- Location.java
+++ Location.java
@@ -1,6 +1,16 @@
 public class Location{
     int x;
     int y;
+    public static final Location above = new Location(1, 0);
+    public static final Location below = new Location(-1, 0);
+    public static final Location left = new Location(0, -1);
+    public static final Location right = new Location(0, 1);
+
+    public static final Location above2 = new Location(2, 0);
+    public static final Location below2 = new Location(-2, 0);
+    public static final Location left2 = new Location(0, -2);
+    public static final Location right2 = new Location(0, 2);
+    
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
