2b24d57b5b4790dfd444f4fd5bd393c1241d55d5
Himanshu Arora <himanshu.arora1111@gmail.com>
2024-05-28 03:03:23 +0300

    The hiding bug :). found ya!

--- Player.java
+++ Player.java
@@ -95,15 +95,15 @@ public class Player {
         out.append("up := (row + 1 < rows &\n"+
 		   "\t(board[row + 1][ col] = \"_\" | board[row + 1 ][col]  = \".\" | \n"+
 		   "\t(boxUp & row + 2 < rows & (board[row + 2][col] = \"_\" | board[row + 2][col] = \".\"))));\n");
-        out.append("down := (row - 1 > 0 &\n"+
+        out.append("down := (row - 1 >= 0 &\n"+
 		   "\t(board[row - 1][ col] = \"_\" | board[row - 1 ][col]  = \".\" |\n" +
 		   "\t(boxDown & row - 2 >= 0 & (board[row - 2][col] = \"_\" | board[row - 2][col] = \".\"))));\n");
         out.append("right := (col + 1 < cols &\n"+
 		   "\t(board[row][col + 1] = \"_\" | board[row][col + 1]  = \".\" | \n"+
 		   "\t(boxRight & col + 2 < cols & (board[row][col + 2] = \"_\" | board[row][col + 2] = \".\"))));\n");
-    	out.append("left := (col - 1 > 0 &\n"+
+    	out.append("left := (col - 1 >= 0 &\n"+
 		   "\t(board[row][col - 1] = \"_\" | board[row][col - 1]  = \".\" | \n"+
-		   "\t(boxRight & col - 2 < cols & (board[row][col - 2] = \"_\" | board[row][col - 2] = \".\"))));\n");
+		   "\t(boxLeft & col - 2 >= 0 & (board[row][col - 2] = \"_\" | board[row][col - 2] = \".\"))));\n");
         
         out.append("ASSIGN\n");
         for (int r = b.rows -1 ; r >= 0 ; r--){
@@ -127,7 +127,7 @@ public class Player {
 				
                 "\tleft & right & up & down		: {l, r, u, d};\n\n" +
 
-				"\tleft & right & up & !down       : {l, r, u};\n" +
+				"\tleft & right & up & !down               : {l, r, u};\n" +
 				"\tleft & right & !up & down 		: {l, r, d};\n" + 
 				"\tleft & !right & up & down 		: {l, u, d};\n" + 
 				"\t!left & right & up & down 		: {u, r, d};\n\n" +
