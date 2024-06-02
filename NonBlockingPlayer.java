5e12584a49bc06aacca7b2a3e0dbe1aee03fd4a1
Himanshu Arora <himanshu.arora1111@gmail.com>
2024-06-02 03:02:55 +0300

    Start working on NonblockingPlayer

---
+++ NonBlockingPlayer.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class NonBlockingPlayer extends Player {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public NonBlockingPlayer(Board b) {
+        super(b);
+    }
+
+    public Set<Location> getBlockingPositions() {
+        HashSet<Location> ret = new HashSet<Location>();
+        Board eyesOnPrize = this.getBoard().eyesOnPrize();
+        Set<Location> floorLocations = eyesOnPrize.getFloorLocations();
+        for (Location loc : floorLocations) {
+            Board singleBox = eyesOnPrize.copy();
+            singleBox.setCell(loc, Board.box);
+            NonBlockingPlayer oneBoxPlay = new NonBlockingPlayer(singleBox);
+        }
+        return ret;
+    }
+
+    public String losingCondition() {
+        return atLeastABoxOnGoal();
+    }
+
+    private String atLeastABoxOnGoal() {
+        StringBuilder str = new StringBuilder();
+        str.append("LTLSPEC NAME atLeastOneBoxOnGoal := G!()");
+        /* for (Location loc : getBoard().getGoalPositions()) */
+        Iterator<Location> iter = getBoard().getGoalPositions().iterator();
+        while (iter.hasNext()) {
+            Location curGoal = iter.next();
+            str.append(Board.boardHas(curGoal, Board.boxOnGoal));
+            if (iter.hasNext()) {
+                str.append(" | ");
+            } else {
+                str.append(");");
+            }
+        }
+        return str.toString();
+    }
+}
