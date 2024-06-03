0d7d42cdd23ceab5cae96db7f483716060f5ba11
Himanshu Arora <himanshu.arora1111@gmail.com>
2024-06-03 22:59:33 +0300

    setup NonBlockingPlayer

--- NonBlockingPlayer.java
+++ NonBlockingPlayer.java
@@ -1,27 +1,60 @@
 import java.util.*;
+import java.io.*;
 
 public class NonBlockingPlayer extends Player {
     String name;
+    Location boxLocation;
     public static void main(String[] args) {
         System.out.println("hello");
     }
 
-    public NonBlockingPlayer(Board b, String name) {
-        super(b);
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
-    public Set<Location> getBlockingPositions() {
-        HashSet<Location> ret = new HashSet<Location>();
+    public Location getBoxLocation() {
+        return this.boxLocation;
+    }
+
+    public NonBlockingPlayer(Board b, String name, Location boxLocation) {
+        super(b);
+        this.name = name;
+        this.boxLocation = boxLocation;
+    }
+
+    public Set<Location> getBlockingPositions() throws IOException, InterruptedException {
+        HashSet<Location> blockingPositions = new HashSet<Location>();
         Board eyesOnPrize = this.getBoard().eyesOnPrize();
         Set<Location> floorLocations = eyesOnPrize.getFloorLocations();
+        Set<NonBlockingPlayer> blockCandidatePlayer = new HashSet<NonBlockingPlayer>();
         for (Location loc : floorLocations) {
-            Board singleBox = eyesOnPrize.copy();
-            singleBox.setCell(loc, Board.box);
-            NonBlockingPlayer oneBoxPlay = new NonBlockingPlayer(singleBox, this.name + loc);
-
+            if (this.getBoard().isCorner(loc)) {
+                blockingPositions.add(loc);
+            } else {
+                Board singleBox = eyesOnPrize.copy();
+                singleBox.setCell(loc, Board.box);
+                NonBlockingPlayer oneBoxPlay = new NonBlockingPlayer(singleBox, this.name + loc, loc);
+                blockCandidatePlayer.add(oneBoxPlay);
+            }
         }
-        return ret;
+
+        for (NonBlockingPlayer p : blockCandidatePlayer) {
+            File smvFile = new File(p.getName() + ".smv");
+            File outFile = new File(p.getName() + ".out");
+            p.writeSmv(smvFile);
+            ModelChecker.checkInteractive(smvFile, p.getBound(), outFile);
+            Play curPlay = Play.readTrace(p.getBoard(), outFile);
+            if (!curPlay.isWin()) {
+                blockingPositions.add(p.getBoxLocation());
+            }
+        }
+        return blockingPositions;
+    }
+
+    // This method will decide the aggresiveness of the starategy, how far will yo
+    // go to find all the blocking positions?
+    public int getBound() {
+        return (int) ((this.getBoard().rows + this.getBoard().cols) * 3);
     }
 
     public String losingCondition() {
@@ -30,8 +63,7 @@ public class NonBlockingPlayer extends Player {
 
     private String atLeastABoxOnGoal() {
         StringBuilder str = new StringBuilder();
-        str.append("LTLSPEC NAME atLeastOneBoxOnGoal := G!()");
-        /* for (Location loc : getBoard().getGoalPositions()) */
+        str.append("LTLSPEC NAME atLeastOneBoxOnGoal := G!(");
         Iterator<Location> iter = getBoard().getGoalPositions().iterator();
         while (iter.hasNext()) {
             Location curGoal = iter.next();
